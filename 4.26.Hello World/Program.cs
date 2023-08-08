50e8ddbd6547c687f37c33e979abcb38c3e81b93
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-08 13:43:01 +0100

    4.26 added

---
+++ 4.26.Hello World/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _4._26.Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! in the Console using Console.WriteLine");
+            System.Diagnostics.Debug.WriteLine("Hello World in the Output Window");
+        }
+    }
+}
