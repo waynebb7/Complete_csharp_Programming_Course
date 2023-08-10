245927b23bf31ea74024631b83a9469e06304d96
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-10 19:16:56 +0100

    added reflection

---
+++ 21.266.Tasks/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Threading.Tasks;
+
+
+namespace _21._266.Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> exampleTask = Task.Run(() => 1);
+            Console.WriteLine(exampleTask.Result);
+        }
+    }
+}
---
+++ 25.325.Reflection/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+
+namespace _25._325.Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Member Info
+            Type t = typeof(int);
+
+            MemberInfo[] typeMembers = t.GetMembers();
+            foreach (MemberInfo memberInfo in typeMembers)
+            {
+                Debug.WriteLine("Member = " + memberInfo);
+            }
+
+
+            // Field Info
+            Type f = typeof(int);
+
+            FieldInfo[] typeFields = f.GetFields();
+            foreach (FieldInfo fieldInfo in typeFields)
+            {
+                Debug.WriteLine("Field = " + fieldInfo);
+            }
+
+
+            // Method Info
+            Type m = typeof(int);
+
+            MethodInfo[] typeMethods = m.GetMethods();
+            foreach (MethodInfo methodInfo in typeMethods)
+            {
+                Debug.WriteLine("Method = " + methodInfo);
+            }
+
+            // Generic Types
+            System.Diagnostics.Debug.WriteLine("Is String a generic type: " + typeof(string).IsGenericType);
+            System.Diagnostics.Debug.WriteLine("Is List a generic type: " + typeof(List<>).IsGenericType);
+
+            // Example of generic types
+            Type t1 = typeof(Dictionary<,>);
+            if (t1.IsGenericType)
+            {
+                Type[] typeParameters = t1.GetGenericArguments();
+                Debug.WriteLine("Number of type parameters = {0}", typeParameters.Length);
+                foreach (Type tParam in typeParameters)
+                {
+                    if (tParam.IsGenericParameter)
+                    {
+                        Debug.WriteLine("Generic Type = {0}", tParam);
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Type = {0}", tParam);
+                    }
+                }
+            }
+        }
+    }
+}
