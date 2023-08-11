533750fa9bf87b17a41b1256f757f9bf265c78b7
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-11 13:49:25 +0100

    added Constructors

---
+++ 9.108.Constructors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace _9._108.Constructors
+{
+    internal class Program
+    {
+        int exampleNumber;
+        string exampleName;
+
+        // Constructor: used to initialize variable int exampleNumber with whatever is passed in from Main()
+        public Program(int num)
+        {
+            exampleNumber = num;
+        }
+
+        // An overloaded Constructor
+        public Program(int num, string name)
+        {
+            exampleNumber = num;
+            exampleName = name;
+        }
+
+        static void Main(string[] args)
+        {
+            Program ex = new Program(5);
+            Program ex2 = new Program(5, "Wayne");
+            Debug.WriteLine(ex.exampleNumber);
+            Debug.WriteLine(ex2.exampleNumber, ex2.exampleName);
+        
+        }
+    }
+}
