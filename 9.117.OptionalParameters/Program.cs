9fa958f5235c88ae6d76e64a035de51e1fe890a5
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-11 16:24:38 +0100

    added Infinite Parameters and Optional Parameters

---
+++ 9.117.OptionalParameters/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _9._117.OptionalParameters
+{
+    internal class Program
+    {
+
+        static void PrintMessage(string message = "Hello World")
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            PrintMessage();
+            PrintMessage("My Message");
+        }
+    }
+} 
