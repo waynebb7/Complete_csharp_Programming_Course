642dbf6796972cadd5ff579e330797f9e363ef11
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-11 14:02:02 +0100

    updated

---
+++ 9.109.Properties/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _9._109.Properties
+{
+    internal class Program
+    {
+        //private int myVar1;
+
+        //public int MyVar1 { get => myVar1; set => myVar1 = value; }
+
+        private string id;
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.id = "1234";
+            System.Diagnostics.Debug.WriteLine(program.Id);
+        }
+    }
+}
