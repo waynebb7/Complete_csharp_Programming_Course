d879f393b179fffef838a6f388a2b932a0109370
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-16 14:34:24 +0100

    add WPFObservableCollection

--- WPFObservableCollections/MainWindow.xaml.cs
+++ WPFObservableCollections/MainWindow.xaml.cs
@@ -1,28 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFObservableCollections
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            DataContext = this;
+            entries = new ObservableCollection<string>();
             InitializeComponent();
+        }
+
+        private ObservableCollection<string> entries;
+
+        public ObservableCollection<string> Entries
+        {
+            get { return entries; }
+            set { entries = value; }
+        }
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Entries.Add(txtEntry.Text);
+            txtEntry.Clear();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedItem = (string)lvEntries.SelectedItem;
+            Entries.Remove(selectedItem);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            Entries.Clear();
         }
     }
 }
