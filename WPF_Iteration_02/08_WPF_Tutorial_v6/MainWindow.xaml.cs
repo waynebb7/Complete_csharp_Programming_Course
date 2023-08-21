a5f0bda2cf0e6f7bc2488083f65e208087237eee
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-21 17:25:09 +0100

    add 08_WPF_Tutorial reworks plus instructions

---
+++ WPF_Iteration_02/08_WPF_Tutorial_v6/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace _08_WPF_Tutorial_v6
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string _boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return _boundText; }
+            set
+            {
+                _boundText = value;
+                OnPropertyChanged("BoundText");
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "set from code";
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
