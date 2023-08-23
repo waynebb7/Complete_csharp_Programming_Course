a67d0e3bb847bbf5024db17c4fc840b8e3f3c77a
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-23 20:31:38 +0100

    add more tutorial docs

---
+++ WPF_Iteration_02/09_WPF_Tutorial_v3/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace _09_WPF_Tutorial_v3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Your message here",
+                            "ERROR",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+
+        }
+
+        private void btnFireWithResponse_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you agree?",
+                                                      "Agreement",
+                                                      MessageBoxButton.YesNo,
+                                                      MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                tbInfo.Text = "Agreed";
+            }
+            else
+            {
+                tbInfo.Text = "Not Agreed";
+            }
+        }
+    }
+}
---
+++ WPF_Iteration_02/10_WPF_Tutorial_v1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace _10_WPF_Tutorial_v1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            // Add a filter
+            fileDialog.Filter = "C# Source Files | *.cs";
+            //fileDialog.InitialDirectory = "C:\\temp";
+            fileDialog.Title = "Please pick CS source file(s)...";
+            fileDialog.Multiselect = true;
+
+            bool? success = fileDialog.ShowDialog();
+
+            if (success == true)
+            {
+                // Do something
+                //string path = fileDialog.FileName;
+                //string fileName = fileDialog.SafeFileName;
+
+                string[] paths = fileDialog.FileNames;
+                string[] fileNames = fileDialog.SafeFileNames;
+
+                //tbInfo.Text = path;
+                //tbInfo.Text = fileName;
+            }
+            else
+            {
+                // They didn't pick anything
+            }
+        }
+    }
+}
