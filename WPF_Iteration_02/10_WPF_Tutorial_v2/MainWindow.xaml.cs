3ca5bfdb827e4815dc45de5278c1cf4d8982b136
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-24 08:10:54 +0100

    add next tutorial

---
+++ WPF_Iteration_02/10_WPF_Tutorial_v2/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace _10_WPF_Tutorial_v2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "C# Source Files | *.cs";
+            fileDialog.InitialDirectory = "C:\\temp";
+            fileDialog.Title = "Please select C# Source File(s)...";
+            fileDialog.Multiselect = true;
+
+            bool? success = fileDialog.ShowDialog();
+
+            if (success == true)
+            {
+                // Do something
+                string[] paths = fileDialog.FileNames;
+                string[] fileNames = fileDialog.SafeFileNames;
+
+                //tbInfo.Text = fileName;
+            }
+            else
+            {
+                // They didn't pick anything
+            }
+        }
+    }
+}
