67e1df9eb5232c970a99eb83a84fd88dc60df252
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-24 14:21:49 +0100

    add more tutorial data ref: ListView

---
+++ WPF_Iteration_02/11_WPF_Tutorial_v1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using WinForms = System.Windows.Forms;
+
+namespace _11_WPF_Tutorial_v1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
+            dialog.InitialDirectory = "C:\\_csc_projects\\Complete_csharp_Programming_Course\\WPF_Iteration_02\\11_WPF_Tutorial_v1";
+            WinForms.DialogResult result = dialog.ShowDialog();
+
+            if(result == WinForms.DialogResult.OK)
+            {
+                // Do something with the result (OK)
+                string folder = dialog.SelectedPath;
+
+                // Returns the folder string and displays in MessageBox
+                WinForms.MessageBox.Show(folder);
+            }
+            else
+            {
+                // Do something else if Cancelled
+            }
+        }
+    }
+}
---
+++ WPF_Iteration_02/12_WPF_Tutorial_v1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Windows;
+
+namespace _12_WPF_Tutorial_v1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Add(txtEntry.Text);
+            txtEntry.Clear();
+            txtEntry.Focus();
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            //object item = lvEntries.SelectedItem;
+            var items = lvEntries.SelectedItems;
+
+            var result = MessageBox.Show($"Are you sure you want to delete {items.Count} items?", "Sure", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                var itemsList = new ArrayList(items);
+                foreach (var item in itemsList)
+                {
+                    lvEntries.Items.Remove(item);
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Clear();
+        }
+    }
+}
