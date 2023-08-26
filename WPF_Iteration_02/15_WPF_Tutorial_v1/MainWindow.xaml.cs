2d5df2b2c23f08d4f6b7c3007f0bd4ce2311cc4c
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-26 08:23:13 +0100

    add GridSplitter example

---
+++ WPF_Iteration_02/15_WPF_Tutorial_v1/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace _15_WPF_Tutorial_v1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
