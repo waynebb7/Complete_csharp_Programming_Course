aba416065cea776d82996b72389252b8a9d0d6af
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-26 08:03:56 +0100

    add ObservableCollection tutorial

---
+++ WPF_Iteration_02/13_WPF_Tutorial_v1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace _13_WPF_Tutorial_v1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            Entries = new ObservableCollection<string>();
+            InitializeComponent();
+        }
+
+        private ObservableCollection<string> _entries;
+
+        public ObservableCollection<string> Entries
+        {
+            get { return _entries; }
+            set { _entries = value; }
+        }
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Entries.Add(txtEntry.Text);
+            txtEntry.Clear();
+            txtEntry.Focus();
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedItem = (string)lvEntries.SelectedItem;
+            Entries.Remove(selectedItem);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            Entries.Clear();
+        }
+    }
+}
