9de063bee5ff98d477628cecd1f69c905514acda
Wayne Barrass-Brown <waynebb7@gmail.com>
2023-08-31 13:02:38 +0100

    add more tutorial data about WPF

---
+++ WPF_Iteration_01/16_WPFExpander/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace WPFExpander
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDetails_Click(object sender, RoutedEventArgs e)
+        {
+            expanderDetails.IsExpanded = !expanderDetails.IsExpanded;
+        }
+    }
+}
---
+++ WPF_Iteration_01/19_WPFOpeningCustomWindows/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using WPFOpeningCustomWindows.View;
+
+
+namespace WPFOpeningCustomWindows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            NormalWindow normalWindow = new NormalWindow();
+            normalWindow.Show();
+        }
+
+        private void btnModal_Click(object sender, RoutedEventArgs e)
+        {
+            ModalWindow modalWindow = new ModalWindow(this);
+            Opacity = 0.4;
+            modalWindow.ShowDialog();
+            Opacity = 1;
+            if (modalWindow.Success)
+            {
+                txtInput.Text = modalWindow.Input;
+            }
+        }
+    }
+}
